bf8cc39201cb73e2ccd083b3bae14e946166fda9
unknown <ido.bandd@gmail.com>
2023-05-12 12:16:48 +0300

    added about details

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,27 @@
+import { flexCenter } from "../_mixin"
+import { ReactIcon, TypeScriptIcon, ViteIcon, UbuntuIcon, AWSIcon, NodeJSIcon, ExpressIcon, PostreSQLIcon } from "./Icons"
+const aboutHeader = `${flexCenter}  w-full text-3xl my-3`
+const aboutText = `flex items-center w-full lg:text-xs flex-wrap`
 const About = () => {
   return (
-    <div>About</div>
+    <main className={`flex justify-start flex-col w-full`}>
+      <h1 className={`${aboutHeader}`}>
+        Frontend
+      </h1>
+      <pre className={`${aboutText}`}><ReactIcon className={``} /> - React</pre>
+      <pre className={`${aboutText}`}><ViteIcon className={``} /> - Vite</pre>
+      <pre className={`${aboutText}`}><TypeScriptIcon className={``} /> - TypeScript</pre>
+      <h1 className={`${aboutHeader}`}>
+        Backend
+      </h1>
+      <pre className={`${aboutText}`}><AWSIcon className={``} /> - Amazon Web Services, EC2 Ubuntu</pre>
+      <pre className={`${aboutText}`}><NodeJSIcon className={``} /> - Node.JS</pre>
+      <pre className={`${aboutText}`}><ExpressIcon className={``} /> - Express</pre>
+      <h1 className={`${aboutHeader}`}>
+        Database
+      </h1>
+      <pre className={`${aboutText}`}><PostreSQLIcon className={``} /> - PostreSQL</pre>
+    </main>
   )
 }
 
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -10,7 +10,7 @@ export default function Modal({onClose, children, className}: ModalProps) {
     return ReactDom.createPortal(
         <>
                     <div className={`fixed bottom-0 top-0 left-0 right-0 bg-light/75 ${flexCenter}`} onClick={onClose} >
-                        <div className={`${flexCenter} w-6/12 p-10 bg-dark text-light relative rounded-lg 
+                        <div className={` w-6/12 p-10 bg-dark text-light relative rounded-lg 
                         lg:w-10/12
                         ${className}`}
                         onClick={(e) => e.stopPropagation()}
