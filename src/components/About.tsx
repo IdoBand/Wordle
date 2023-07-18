9c4f805385c57c93ba89807d7c686416beb383bc
unknown <ido.bandd@gmail.com>
2023-07-18 14:44:54 +0300

    updated About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,5 @@
 import { flexCenter } from "../_mixin"
-import { ReactIcon, TypeScriptIcon, ViteIcon, UbuntuIcon, AWSIcon, NodeJSIcon, ExpressIcon, PostreSQLIcon } from "./Icons"
+import { ReactIcon, TypeScriptIcon, ViteIcon, AWSIcon, NodeJSIcon, ExpressIcon, PostreSQLIcon } from "./Icons"
 const aboutHeader = `${flexCenter}  w-full text-3xl my-3`
 const aboutText = `flex items-center w-full lg:text-xs flex-wrap my-1`
 const About = () => {
@@ -14,9 +14,9 @@ const About = () => {
       <h1 className={`${aboutHeader}`}>
         Backend
       </h1>
-      <pre className={`${aboutText}`}><AWSIcon className={``} /> - Amazon Web Services, EC2 Ubuntu</pre>
       <pre className={`${aboutText}`}><NodeJSIcon className={``} /> - Node.JS</pre>
       <pre className={`${aboutText}`}><ExpressIcon className={``} /> - Express</pre>
+      <pre className={`${aboutText}`}><TypeScriptIcon className={``} /> - TypeScript</pre>
       <h1 className={`${aboutHeader}`}>
         Database
       </h1>
